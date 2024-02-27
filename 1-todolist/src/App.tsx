a2fdfd09b4163935930fdcb30630a6ec85e09aa3
Antonio Klenitsky <a_klenitsky@mail.ru>
2024-02-27 10:01:35 +0300

    add new repos

--- 1-todolist/src/App.tsx
+++ 1-todolist/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {TaskProps, Todolist} from "./Todolist";
+
 
 function App() {
+    const truckJan1 = 'What to learn-1'
+    const truckJan2 = 'What to learn-2'
+    const truckFeb1 = 'What to learn-3'
+    // const trucks=['What to learn-1','What to learn-2','What to learn-3']
+    const trucks = {
+        truckJan1: 'What to learn-1',
+        truckJan2: 'What to learn-2',
+        truckFeb1: 'What to learn-3'
+    }
+
+    const tasks1:TaskProps[] = [
+        {id: 1, title: 'HTML&CSS', isDone: true},//0
+        {id: 2, title: 'JS', isDone: true},//1
+        {id: 3, title: 'ReactJS', isDone: false},//2
+    ]
+
+
+    const tasks2:TaskProps[] = [
+        // {id: 1, title: 'Hello world', isDone: true},
+        // {id: 2, title: 'I am Happy', isDone: false},
+        // {id: 3, title: 'Yo', isDone: false},
+        // {id: 4, title: 'Yo222222', isDone: false},
+    ]
+    const arr=[1,2,3]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist truck={trucks.truckJan1} truck2={trucks.truckJan2} tasks={tasks1}/>
+            <Todolist truck={trucks.truckFeb1} tasks={tasks2}/>
         </div>
     );
 }
